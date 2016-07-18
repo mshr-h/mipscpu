8130c8b436772df021b5273c7e1d5c4aacdd4581
mshr-h <mshr-h@users.noreply.github.com>
2016-07-18 11:56:39 +0900

    much richer print

--- iss/main.go
+++ iss/main.go
@@ -13,6 +13,19 @@ const (
 	BASE_MASK = addr(0x7F00)
 )
 
+func findInstructionName(i Instruction, table *map[string]InstructionFormat) string {
+	for k, v := range *table {
+		if v.opcode == i.opcode() {
+			if v.function != IGNORE && v.function == i.function() {
+				return k
+			} else if v.function == IGNORE {
+				return k
+			}
+		}
+	}
+	return ""
+}
+
 func main() {
 	var binfile = "fibonacci.o"
 	if len(os.Args) >= 2 {
@@ -25,11 +38,12 @@ func main() {
 	}
 
 	var cpu = NewCpu(bin)
-	fmt.Printf("  inst   op rd rt rs\n")
+	fmt.Printf("  inst    opcd rd rt rs   imm  address\n")
 	for i := 0; i < 0xA; i++ {
 		for j := 0; j < 0x10; j += 4 {
 			var i = Instruction(cpu.instMemory.ReadWord(addr(0x10*i + j)))
-			fmt.Printf("%08x %02x %2d %2d %2d\n", i, i.opcode(), i.rd(), i.rt(), i.rs())
+			var mnemonic = findInstructionName(i, cpu.instTable)
+			fmt.Printf("%08x %5s %2d %2d %2d %5d %8d\n", i, mnemonic, i.rd(), i.rt(), i.rs(), i.immediate(), i.address())
 		}
 	}
 }
